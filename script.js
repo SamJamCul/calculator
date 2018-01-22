c4aa6f6ef2299c1c6f0a228661f34450236b9859
SamJamCul <samueljamesculley@gmail.com>
2018-01-22 14:17:25 +0000

    almost finished really, basic logic all ironed out

--- script.js
+++ script.js
@@ -1,35 +1,101 @@
+const numbers = document.querySelectorAll('.button');
+const operations = document.querySelectorAll('.opebut');
+const clear = document.querySelector('#clear');
+const cancel = document.querySelector('#cancel');
+const equals = document.querySelector('.equals');
+const display = document.querySelector('.display');
+
+let answer = '';
+let operation = '';
+let firstnum = '';
+let secondnum = '';
+
 function add(a, b) {
-  return a + b;
+  return parseFloat(a) + parseFloat(b);
 }
 
 function subtract(a, b) {
-  return a - b;
+  return parseFloat(a) - parseFloat(b);
 }
 
 function multiply(a, b) {
-  return a * b;
+  return parseFloat(a) * parseFloat(b);
 }
 
 function divide(a, b) {
-  return a / b;
+  return parseFloat(a) / parseFloat(b);
 }
 
 function operate(o, a, b) {
   return o(a, b);
 }
 
-const numbers = document.querySelectorAll('.button');
-const operations = document.querySelectorAll('.opebut');
-const metaButtons = document.querySelectorAll('.metabut');
-const equals = document.querySelectorAll('.equals');
-const display = document.querySelector('.display');
-
-let answer = '';
+function equal() {
+  if (display.hasChildNodes()) {
+    display.removeChild(display.childNodes[0]);
+  }
+  if(operation !== ''){
+    if(secondnum !== ''){
+      answer = operate(eval(operation), firstnum, secondnum)
+      display.textContent = answer;
+    }
+  }
+  operation = secondnum = '';
+  firstnum = answer;
+  display.textContent = answer;
+}
 
 numbers.forEach((button) => {
   button.addEventListener('click', (e) => {
-    display.removeChild(display.childNodes[0])
-    answer += button.textContent
-    display.textContent = answer;
+    if (display.hasChildNodes()) {
+      display.removeChild(display.childNodes[0]);
+    }
+    if(operation === '' && answer === ''){
+      firstnum += button.textContent
+      display.textContent = firstnum;
+    } else if (operation === ''){
+      if (display.hasChildNodes()) {
+        display.removeChild(display.childNodes[0]);
+      }
+      answer = firstnum = ''
+      firstnum += button.textContent
+      display.textContent = firstnum;
+    } else if(operation !== '') {
+      secondnum += button.textContent
+      display.textContent = secondnum;
+    }
   })
+})
+
+operations.forEach((button => {
+  button.addEventListener('click', (e) => {
+    if(secondnum === '') {
+      operation = button.id
+    } else if (secondnum !== '') {
+      equal()
+      operation = button.id
+    }
+  })
+}))
+
+equals.addEventListener('click', equal)
+
+clear.addEventListener('click', function(){
+  if (display.hasChildNodes()) {
+    display.removeChild(display.childNodes[0]);
+  }
+  operation = firstnum = secondnum = answer = '';
+})
+
+cancel.addEventListener('click', function(){
+  if (display.hasChildNodes()) {
+    display.removeChild(display.childNodes[0]);
+  }
+  if (secondnum !== '') {
+    secondnum = secondnum.slice(0, -1)
+    display.textContent = secondnum
+  } else if (firstnum !== '') {
+    firstnum = firstnum.slice(0, -1)
+    display.textContent = firstnum
+  }
 })
